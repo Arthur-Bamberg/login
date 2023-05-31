a867263cffb2ba17fa15369af8fcf3f75ac6be55
Arthur-Bamberg <bamberguisses@gmail.com>
2023-05-30 22:17:19 -0300

    feat: updated sql dump database

--- database/dump.sql
+++ database/dump.sql
@@ -1,6 +1,64 @@
-CREATE TABLE user (
-  idUser INT PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  password VARCHAR(255) NOT NULL
-);
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.27-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.4.0.6659
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for primeiro_trimestre
+CREATE DATABASE IF NOT EXISTS `primeiro_trimestre` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `primeiro_trimestre`;
+
+-- Dumping structure for table primeiro_trimestre.role
+CREATE TABLE IF NOT EXISTS `role` (
+  `idRole` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `description` varchar(255) NOT NULL DEFAULT '',
+  `authorization` varchar(50) NOT NULL DEFAULT '',
+  `isActive` tinyint(4) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`idRole`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table primeiro_trimestre.role: ~0 rows (approximately)
+
+-- Dumping structure for table primeiro_trimestre.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `mediaUrl` varchar(255) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` bigint(20) DEFAULT NULL,
+  `isActive` tinyint(4) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`idUser`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table primeiro_trimestre.user: ~1 rows (approximately)
+INSERT INTO `user` (`idUser`, `username`, `name`, `mediaUrl`, `email`, `password`, `phone`, `isActive`) VALUES
+	(1, 'teste', '', '', 'teste@a.com', 'fe703d258c7ef5f50b71e06565a65aa07194907f', NULL, 0);
+
+-- Dumping structure for table primeiro_trimestre.user_role
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `FK_idUser` int(11) NOT NULL,
+  `FK_idRole` int(11) NOT NULL,
+  `isActive` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table primeiro_trimestre.user_role: ~0 rows (approximately)
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
